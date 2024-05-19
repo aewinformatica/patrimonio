7e350ebab122be67d3c2e072b99791d1d5bc5a0f
Wagner G Jesus <wagner.jesus@esmenezes.com.br>
2024-05-19 15:13:58 -0300

    password encrypt and access h2-console

--- patrimonio-api/src/main/java/br/com/aewinformatica/patrimonio/config/BasicSecurityConfig.java
+++ patrimonio-api/src/main/java/br/com/aewinformatica/patrimonio/config/BasicSecurityConfig.java
@@ -8,13 +8,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,15 +28,24 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig{
-			
+		
 	@Bean
 	CorsConfigurationSource apiConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-	    configuration.setAllowedMethods(Arrays.asList("POST", "GET", "DELETE", "PUT", "OPTIONS"));
-	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
-	    configuration.setAllowCredentials(true);
-	    configuration.setMaxAge((long) 3600);
+
+	    configuration.setAllowedMethods(Arrays.asList("*"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    configuration.setExposedHeaders(Arrays.asList("*"));
+	    /*When allowCredentials is true, 
+	    allowedOrigins cannot contain the special value "*" 
+	    since that cannot be set on the "Access-Control-Allow-Origin" 
+	    response header. To allow credentials to a set of origins, 
+	    list them explicitly or consider using "allowedOriginPatterns" instead.
+	    */
+	    configuration.setAllowCredentials(false);
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+
 	  
 	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	    source.registerCorsConfiguration("/**", configuration);
@@ -42,10 +55,20 @@ public class BasicSecurityConfig{
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    			http.cors(cors -> cors.configurationSource(apiConfigurationSource()))
-	            .authorizeHttpRequests((authz) -> authz
+    
+    			http
+    			.cors(cors -> cors.configurationSource(apiConfigurationSource()))
+    			.csrf(AbstractHttpConfigurer::disable)
+    			.sessionManagement(manager -> manager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).authorizeHttpRequests((authz) -> authz
 	                .anyRequest().authenticated()
-	            ).httpBasic(Customizer.withDefaults());
+	             ).httpBasic(Customizer.withDefaults())
+	             .formLogin(Customizer.withDefaults())
+	             .headers(header -> header
+    					.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+    					.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+    			 );
 	        
 	        return http.build();
 	    }
@@ -55,17 +78,15 @@ public class BasicSecurityConfig{
     UserDetailsService userDetailsService() {
 		UserDetails user =
 			 User.withUsername("aewinformatica")
-				.password("password")
+				.password(passwordEncoder().encode("password"))
 				.roles("ADMIN")
 				.build();
+//		System.out.println(user);
 
 		return new InMemoryUserDetailsManager(user);
 	}
     
-    @Bean
-    PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-    	return NoOpPasswordEncoder.getInstance();
-    }
 
+    @Bean
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 }
